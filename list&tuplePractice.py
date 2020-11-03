435f7567ac40d72d6a00c59e72b1c12bf2a85660
selma_eghbali <73425555+selmoosh@users.noreply.github.com>
2020-11-03 12:42:26 +0330

    selma eghbali

--- list&tuplePractice.py
+++ list&tuplePractice.py
@@ -12,3 +12,10 @@ for n in numbers:
         break
     count +=1
     print(count)
+#---------------------------------
+#selma
+numbers= [2,4,6,8,12,14,16,("test","pre-test"),18]
+
+count = list(map(type, numbers)).index(tuple)
+
+print(count)
